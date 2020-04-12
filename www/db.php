3232fe3bf64d06ad1d9b5ddb0197cf559a1dc7b5
MiftahKJ <rodri.ant48s@gmail.com>
2020-04-12 20:45:34 +0700

    first

---
+++ www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id12789061_miptahonhom","tugaswebservice","id12789061_restfull") or die ("could not connect database");
+?>
